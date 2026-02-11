b06a68da3ee0007178a06cf690e8ccdc0575301d
Jahongir <jahongir7771503@gmail.com>
2026-02-11 17:40:11 +0500

    netlify function fix

--- netlify/functions/order.js
+++ netlify/functions/order.js
@@ -1,26 +1,25 @@
-export async function handler(event) {
-  try {
+exports.handler = async function (event) {
+
     const data = JSON.parse(event.body);
 
-    const response = await fetch("http://46.225.115.121:5000/webapp-order", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    });
+    try {
+        await fetch("http://46.225.115.121:5000/webapp-order", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
 
-    const result = await response.json();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ status: "ok" })
+        };
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result)
-    };
-
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Server error" })
-    };
-  }
-}
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ status: "error" })
+        };
+    }
+};
